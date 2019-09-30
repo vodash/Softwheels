4ff77aeb61981832fbd44f4bcac63b79a8d00064
Vodash <k.udema@st.hanze.nl>
2019-09-30 12:34:35 +0200

    fixed indents

--- openyourmind/src/app/app-routing.module.ts
+++ openyourmind/src/app/app-routing.module.ts
@@ -1,21 +1,23 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {LoginComponent} from "./login/login.component";
 
 // router module for navigation in the web app
 const routes: Routes = [
-  { path: '',
-    component: HomeComponent
-  },
-  {
-    path: 'login',
-    component: LoginComponent
-  }
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- openyourmind/src/app/app.component.ts
+++ openyourmind/src/app/app.component.ts
@@ -1,44 +1,44 @@
-import { Component, HostBinding, Input, Output } from '@angular/core';
-import { trigger, state, style, transition, animate} from '@angular/animations';
+import {Component, HostBinding, Input, Output} from '@angular/core';
+import {trigger, state, style, transition, animate} from '@angular/animations';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  animations: [
-    trigger('slideInOut', [
-      state('out', style({
-        transform: 'translate3d(0, 0, 0)'
-      })),
-      state('in', style({
-        transform: 'translate3d(-100%, 0, 0)'
-      })),
-      transition('in => out', animate('400ms ease-in-out')),
-      transition('out => in', animate('400ms ease-in-out'))
-    ]),
-  ]
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    animations: [
+        trigger('slideInOut', [
+            state('out', style({
+                transform: 'translate3d(0, 0, 0)'
+            })),
+            state('in', style({
+                transform: 'translate3d(-100%, 0, 0)'
+            })),
+            transition('in => out', animate('400ms ease-in-out')),
+            transition('out => in', animate('400ms ease-in-out'))
+        ]),
+    ]
 })
 export class AppComponent {
-  title = 'Open your mind';
+    title = 'Open your mind';
 
-  menuState = 'in';
-  @HostBinding('class.change') someField = false;
+    menuState = 'in';
+    @HostBinding('class.change') someField = false;
 
-  onHide(val: string) {
-    this.menuState = val;
-  }
+    onHide(val: string) {
+        this.menuState = val;
+    }
 
-  ngInit() {
-    this.someField = true; // set class `someClass` on `<body>`
-  }
+    ngInit() {
+        this.someField = true; // set class `someClass` on `<body>`
+    }
 
-  toggleMenu() {
-    this.menuState = (this.menuState === 'out') ? 'in' : 'out';
-    this.someField = (this.menuState === 'out') ? true : false;
-  }
+    toggleMenu() {
+        this.menuState = (this.menuState === 'out') ? 'in' : 'out';
+        this.someField = (this.menuState === 'out') ? true : false;
+    }
 
-  forceCloseMenu() {
-    this.menuState = 'in';
-    this.someField = false;
-  }
+    forceCloseMenu() {
+        this.menuState = 'in';
+        this.someField = false;
+    }
 }
