e559c14ecd591eae6e76e5f61369ea99abda1b6b
Vodash <k.udema@st.hanze.nl>
2019-10-11 12:44:22 +0200

    Started working on the patient profile page
    started basics of the patient profile page.

--- openyourmind/src/app/app-routing.module.ts
+++ openyourmind/src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {ChartComponent} from './chart/chart.component';
-import { DoctorProfileComponent } from './doctor-profile/doctor-profile.component';
+import {DoctorProfileComponent} from './doctor-profile/doctor-profile.component';
+import {PatientProfileComponent} from './patient-profile/patient-profile.component';
 
 // router module for navigation in the web app
 const routes: Routes = [
@@ -17,6 +18,10 @@ const routes: Routes = [
     {
         path: 'profile',
         component: DoctorProfileComponent
+    },
+    {
+        path: 'patient',
+        component: PatientProfileComponent
     }
 ];
 
