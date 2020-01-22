5633df86a58b12c9d460a1e4c6bf45922db6401a
Chris Roscher <chris@aitai.nl>
2020-01-22 16:35:09 +0100

    Change graph labels

--- openyourmind/src/app/chart/chart.component.ts
+++ openyourmind/src/app/chart/chart.component.ts
@@ -57,11 +57,11 @@ export class ChartComponent implements OnInit {
     //     },
         series: [
             {
-                name: 'Activity',
+                name: 'Steps',
                 data: []
             },
             {
-                name: 'Sleep',
+                name: 'Minutes Sedentary',
                 data: []
             }
         ]}
